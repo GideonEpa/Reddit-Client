dc9ca7235cc08726f1225f3a1775cd65f650dfd1
GideonEpa <gideon.epa@outlook.com>
2024-07-31 12:15:46 +1000

    Created upvote functionality

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,9 +2,9 @@ import styles from './NavBar.module.css';
 import { useState } from 'react';
 
 function NavBar() {
-    const [ input, setInput ] = useState("")
-    
-    function handleChange({target}) {
+    const [input, setInput] = useState("")
+
+    function handleChange({ target }) {
         setInput(target.value);
     }
 
@@ -13,17 +13,17 @@ function NavBar() {
             <div className={styles.brand}>
                 <img
                     src='https://pluspng.com/img-png/reddit-logo-png-white-reddit-icon-free-white-site-logo-icons-512x512.png'
-                    className={styles.img}/>
+                    className={styles.img} />
                 <h1>Reddit <span>Lite</span></h1>
             </div>
             <div className={styles.search}>
                 <input
                     type="text"
                     value={input}
-                    onChange={handleChange}/>
-                <img 
+                    onChange={handleChange} />
+                <img
                     src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg"
-                    className={styles.img}/>
+                    className={styles.img} />
             </div>
         </div>
     )
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,46 +2,61 @@ import styles from './Post.module.css';
 import { useState } from 'react';
 import Comments from '../comments/Comments.js';
 
-function Post({post}) {
-    const { 
+function Post({ post }) {
+    const {
         author,
-        num_comments, 
-        ups, 
-        title, 
+        num_comments,
+        ups,
+        title,
         subreddit,
         url,
         created_utc,
-        permalink 
+        permalink
     } = post;
 
     const timePosted = Math.floor((((new Date()).getTime() / 1000) - created_utc) / 60 / 60);
 
     const [showComments, setShowComments] = useState(false);
-
-    function handleClick(){
+    function handleClick() {
         setShowComments(showComments ? false : true);
     }
-    
+
+    const [upvoted, setUpvoted] = useState(false);
+    function toggleUpvote() {
+        upvoted ? setUpvoted(false) : setUpvoted(true);
+    }
+
+    let path;
+    upvoted ? path = "m479-60-87-79q-108-98-178-168.5T104-434q-40-56-55.5-103.5T33-637q0-113 76-189.5T298-903q50 0 96.5 17.5T479-836q38-32 84.5-49.5T660-903q113 0 190 76.5T927-637q0 51-15.5 98.5t-55.5 103Q816-380 746-309T566-139l-87 79Z" : path = "m479-60-87-79q-108-98-178-168.5T104-434q-40-56-55.5-103.5T33-637q0-112.95 76.24-189.48Q185.47-903 298-903q49.93 0 96.47 17.5Q441-868 479-836q38-32 84.53-49.5Q610.07-903 660-903q113.38 0 190.19 76.52Q927-749.95 927-637q0 51-15.5 98.5t-55.5 103Q816-380 746-309T566-139l-87 79Zm0-183q95-87 155.5-146.5T730-493q35-44 48-77.65 13-33.65 13-66.33Q791-693 753.49-730q-37.5-37-93.01-37-42.48 0-80.98 27-38.5 27-49.5 64H429q-10.53-37-49.76-64-39.24-27-80.97-27-55.54 0-91.91 37Q170-693 170-637q0 33.76 13.44 67.99 13.44 34.24 48 78.63Q266-446 326-387q60 59 153 144Zm2-262Z"
+
     return (
         <div className={styles.post}>
-            <span>r/{subreddit} {ups}</span>
-            <h2>{title}</h2> <br/>
-            {url[8] === "i" && <img 
-                src={url}
-                className={styles.img}
-                alt={title}/>}
-            <div className={styles.container}>
+            <div className={styles.postBody}>
+                <div className={styles.ups}>
+                    <svg onClick={toggleUpvote} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d={path} /></svg>
+                    <p>{ups > 1000 ? Math.floor((upvoted ? ups + 1 : ups) / 1000 * 10) / 10 + "k" : (upvoted ? ups + 1 : ups)}</p>
+                </div>
+                <div>
+                    <span>r/{subreddit}</span>
+                    <h2>{title}</h2> <br />
+                    <img
+                        src={url}
+                        className={styles.img}
+                        alt={title} />
+                </div>
+            </div>
+            <div className={styles.credentials}>
                 <p>by <span className={styles.author}>{author}</span></p>
                 <p> {timePosted} hours ago</p>
                 <p
-                className={styles.commentsCount} 
-                onClick={handleClick}>{num_comments} comments</p>
+                    className={styles.commentsCount}
+                    onClick={handleClick}>{num_comments} comments</p>
             </div>
             <div className={styles.comments}>
                 {showComments && <Comments
                     permalink={permalink}
                     showComments={showComments}
-                    handleClick={handleClick}/>}
+                    handleClick={handleClick} />}
             </div>
         </div>
     )
